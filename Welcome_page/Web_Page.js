2cd75f07d7b43c07d9766e3b99815bdee23d843b
Yohan_Perera <syohanperera74@gmail.com>
2024-12-31 00:23:31 +0530

    Complete welcome page

--- Welcome_page/Web_Page.js
+++ Welcome_page/Web_Page.js
@@ -13,6 +13,25 @@ document.addEventListener('click', function (event) {
     dropdownMenu.style.display = 'none';
   }
 });
+//New Design icons
+const imageContainers = document.querySelectorAll('.image-container');
+
+imageContainers.forEach(container => {
+    const image = container.querySelector('.image');
+    const textOverlay = container.querySelector('.text-overlay');
+
+    container.addEventListener('mouseenter', () => {
+        image.style.transform = 'scale(1.1)'; 
+        image.style.opacity = '0.8';
+        textOverlay.style.display = 'block';
+    });
+
+    container.addEventListener('mouseleave', () => {
+        image.style.transform = 'scale(1)'; 
+        image.style.opacity = '1'; 
+        textOverlay.style.display = 'none';
+    });
+});
 
           
   
